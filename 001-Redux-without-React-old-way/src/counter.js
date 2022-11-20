2dfb97a9b6b2b742bbe092a5603c7624f6bca2a0
Nikodem Graczewski <ngraczewski.sda@gmail.com>
2022-11-20 08:44:15 +0100

    feat(001): prepared for lecture

--- 001-Redux-without-React-old-way/src/counter.js
+++
@@ -1,9 +0,0 @@
-export const counter = `
-    <div class="counter-container">
-        <div class="counter">0</div>
-        <div class="counter-actions">
-            <button class="button counter-increment">Increment</button>
-            <button class="button counter-decrement">Decrement</button>
-        </div>
-    </div>
-`;
