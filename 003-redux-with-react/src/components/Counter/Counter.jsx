f161b370b87f723377618b7baee6b3e450e2114c
Nikodem Graczewski <ngraczewski.sda@gmail.com>
2022-11-20 17:00:41 +0100

    feat(003): solution

--- 003-redux-with-react/src/components/Counter/Counter.jsx
+++ 003-redux-with-react/src/components/Counter/Counter.jsx
@@ -3,14 +3,26 @@ import styles from "./Counter.module.css";
 import { Button } from "../Button/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { counterActions, counterSelectors } from "../../redux/counterSlice";
-import { store } from "../../redux/store";
 
 export const Counter = () => {
   const dispatch = useDispatch(); //store.dispatch
   const counter = useSelector(counterSelectors.selectCounter);
+  const incrementsCount = useSelector(counterSelectors.selectIncrementsCount);
+  const decrementsCount = useSelector(counterSelectors.selectDecrementsCount);
+  const totalOperationsCount = useSelector(
+    counterSelectors.selectTotalOperationsCount
+  );
 
   const handleIncrementClick = () => {
     dispatch(counterActions.increment(1));
+  };
+
+  const handleDecrementClick = () => {
+    dispatch(counterActions.decrement(1));
+  };
+
+  const handleResetClick = () => {
+    dispatch(counterActions.reset());
   };
 
   return (
@@ -18,7 +30,13 @@ export const Counter = () => {
       <div className={styles.counter}>{counter}</div>
       <div>
         <Button onClick={handleIncrementClick}>Increment</Button>
-        {/* <Button onClick={decrement}>Decrement</Button> */}
+        <Button onClick={handleDecrementClick}>Decrement</Button>
+        <Button onClick={handleResetClick}>Reset</Button>
+      </div>
+      <div>
+        <div>Number of increments: {incrementsCount}</div>
+        <div>Number of decrements: {decrementsCount}</div>
+        <div>Total number of operations: {totalOperationsCount}</div>
       </div>
     </div>
   );
--- 003-redux-with-react/src/redux/counterSlice.js
+++ 003-redux-with-react/src/redux/counterSlice.js
@@ -1,20 +1,52 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = 666;
+const initialState = {
+  counter: 100,
+  incrementsCount: 0,
+  decrementsCount: 0,
+};
 
 const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
-    increment: (state, action) => state + action.payload,
-    decrement: (state, action) => state - action.payload,
+    increment: (state, action) => {
+      state.counter += action.payload;
+      state.incrementsCount++;
+    },
+    decrement: (state, action) => ({
+      ...state,
+      counter: state.counter - action.payload,
+      decrementsCount: state.decrementsCount + 1,
+    }),
+    reset: () => initialState,
   },
 });
 
-const selectCounter = (state) => state.counter;
+const selectCounterSlice = (state) => state.counter;
+
+const selectCounter = (state) => selectCounterSlice(state).counter;
+
+const selectIncrementsCount = (state) => {
+  return state.counter.incrementsCount;
+};
+
+const selectDecrementsCount = (state) => {
+  return state.counter.decrementsCount;
+};
+
+const selectTotalOperationsCount = (state) => {
+  const incrementsCount = selectIncrementsCount(state);
+  const decrementsCount = selectDecrementsCount(state);
+
+  return incrementsCount + decrementsCount;
+};
 
 export const counterSelectors = {
   selectCounter,
+  selectIncrementsCount,
+  selectDecrementsCount,
+  selectTotalOperationsCount,
 };
 export const counterActions = counterSlice.actions;
 export const counterReducer = counterSlice.reducer;
