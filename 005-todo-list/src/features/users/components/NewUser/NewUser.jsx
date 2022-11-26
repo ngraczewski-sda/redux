68f599738c4765281ca14e7df15f43c20a6d4b49
Nikodem Graczewski <ngraczewski.sda@gmail.com>
2022-11-26 14:32:07 +0100

    feat(005): added NewUser impl

--- 005-todo-list/src/features/users/components/NewUser/NewUser.jsx
+++ 005-todo-list/src/features/users/components/NewUser/NewUser.jsx
@@ -1,11 +1,34 @@
-// import styles from "./NewUser.module.css";
+import { Button } from "components/Button/Button";
+import { Input } from "components/Input/Input";
+import { useState } from "react";
+import styles from "./NewUser.module.css";
 
 export const NewUser = () => {
+  const [username, setUsername] = useState("");
+
+  const handleUsernameChange = (e) => {
+    const newUsername = e.target.value;
+    setUsername(newUsername);
+  };
+
+  const handleAddClick = () => {
+    if (!username) {
+      return;
+    }
+
+    alert(username);
+  };
+
   return (
-    <div
-    // className={styles.container}
-    >
-      {/* <h3 className={styles.header}>Add user</h3> */}
+    <div className={styles.container}>
+      <h3 className={styles.header}>Add user</h3>
+      <Input
+        id="username"
+        label="Username"
+        value={username}
+        onChange={handleUsernameChange}
+      />
+      <Button onClick={handleAddClick}>Add</Button>
     </div>
   );
 };
