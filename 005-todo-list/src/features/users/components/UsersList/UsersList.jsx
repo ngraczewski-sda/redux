38e18118ad732f4f8a467892a71ff121f49f220d
Nikodem Graczewski <ngraczewski.sda@gmail.com>
2022-11-26 16:15:13 +0100

    feat(005): removed selectors

--- 005-todo-list/src/features/users/components/UsersList/UsersList.jsx
+++ 005-todo-list/src/features/users/components/UsersList/UsersList.jsx
@@ -1,13 +1,6 @@
 // import styles from "./UsersList.module.css";
 
-import { usersSelectors } from "features/users/redux/usersSelectors";
-import { useSelector } from "react-redux";
-
 export const UsersList = () => {
-  const users = useSelector(usersSelectors.selectUsers);
-  const activeUserId = useSelector(usersSelectors.selectActiveUserId);
-
-  console.log({ users, activeUserId });
   return (
     <div
     // className={styles.container}
--- 005-todo-list/src/features/users/redux/usersSelectors.js
+++
@@ -1,7 +0,0 @@
-const selectUsers = (state) => state.users.users;
-const selectActiveUserId = (state) => state.users.activeUserId;
-
-export const usersSelectors = {
-  selectUsers,
-  selectActiveUserId,
-};
